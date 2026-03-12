1bb8876020ed006b190ffbbfa847c31cda365cfb
dmgyzchlry5-source <dmgyzchlry5@gmail.com>
2026-03-12 20:48:02 +0800

    Update forklift_docs_crawler.py

--- forklift_docs_crawler.py
+++ forklift_docs_crawler.py
@@ -1,1 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
+url="https://blog.csdn.net/weixin_45678901/article/details/123456789"
+soup = BeautifulSoup(response.text, 'html.parser')
+print(soup.title.text
